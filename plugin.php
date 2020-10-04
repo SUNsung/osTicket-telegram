95dfc3708d78847c42dc9db7841626c4bfe2fde6
ad-ThinkPad-T430s <weatherius@gmail.com>
2020-10-04 09:13:10 +0300

    namespases

--- plugin.php
+++ plugin.php
@@ -1,11 +1,11 @@
 <?php
 return array(
-    'id' =>             'osticket:telegrambot',
-    'version' =>        '0.3',
-    'name' =>           'Telegram Bot',
-    'author' =>         'Tomas Kirkegaard',
-    'description' =>    'Notify Telegram on new ticket.',
-    'url' =>            'https://github.com/drtomasso',
-    'plugin' =>         'telegrambot.php:TelegramPlugin',
+    'id'          => 'osticket:telegrambot',
+    'version'     => '0.1',
+    'name'        => 'Telegram Bot notifier',
+    'author'      => 'SUNsung',
+    'description' => 'Notify Telegram on new ticket.',
+    'url'         => 'https://github.com/SUNsung/osTicket_telegram',
+    'plugin'      => 'telegrambot.php:TelegramPlugin',
 );
 ?>
